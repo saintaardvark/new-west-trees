3fbda0bdb5ad4244f2198e8428993b80aba72ccb
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-07-02 16:04:39 -0700

    Add queries for all unknown trees, and all known trees
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- gis_query.js
+++ gis_query.js
@@ -2,6 +2,22 @@ var cartoDBUserName = "saintaardvark";
 var queryAllTrees = "SELECT * FROM trees_east";
 var queryAllTreeCommonNames = "SELECT DISTINCT common_name FROM trees_east ORDER BY common_name";
 
+var queryUnknownTrees = `SELECT * FROM trees_east WHERE
+(common_name = '' OR common_name IS NULL) AND
+(genus = '' OR genus IS NULL) AND
+(species = '' OR species IS NULL) AND
+(scientific_name = '' OR scientific_name IS NULL) AND
+(cultivar = '' OR cultivar IS NULL)
+`;
+
+var queryKnownTrees =  `SELECT * FROM trees_east WHERE
+(common_name != '' AND common_name IS NOT NULL) OR
+(genus != '' AND genus IS NOT NULL) OR
+(species != '' AND species IS NOT NULL) OR
+(scientific_name != '' AND scientific_name IS NOT NULL) OR
+(cultivar != '' OR cultivar IS NOT NULL)
+`;
+
 function showAll() {
   maybeClearLayers();
   // Get CARTO selection as GeoJSON & add to map
