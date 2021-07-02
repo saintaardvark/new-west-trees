c998266f9499eee287ddecae56e524924d3c5dee
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-07-02 15:28:57 -0700

    Break out query functions to their own file
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

---
+++ gis_query.js
@@ -0,0 +1,77 @@
+var cartoDBUserName = "saintaardvark";
+var queryAllTrees = "SELECT * FROM trees_east";
+var querySweetgumTrees = "SELECT * FROM trees_east WHERE common_name='SWEETGUM'";
+var queryAllTreeCommonNames = "SELECT DISTINCT common_name FROM trees_east ORDER BY common_name";
+
+function selectTreesMatchingCommonName(common_name) {
+  maybeClearLayers();
+  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + queryAllTrees + " WHERE common_name = '" + common_name + "'", function(data) {
+    nwTrees = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+	if (feature.properties) {
+	  popupMsg = "Genus: "  + feature.properties.genus
+	    + "<br>Cultivar: " + feature.properties.cultivar
+	    + "<br>Species: " + feature.properties.species
+	    + "<br>Scientific Name: " + feature.properties.scientific_name
+	    + "<br>Common Name: " + feature.properties.common_name;
+	  layer.bindPopup(popupMsg);
+	}
+      }
+    });
+    clusters = L.markerClusterGroup({
+      spiderfyOnMaxZoom: false,
+      disableClusteringAtZoom: 18,
+    });
+    clusters.addLayer(nwTrees);
+    map.addLayer(clusters);
+    nwTrees.addTo(map);
+  });
+}
+
+function closestTree() {
+  maybeClearLayers();
+
+  var sqlQueryClosestTrees;
+  if ($('#common_name_list').val() == "") {
+    sqlQueryClosestTrees = "SELECT * FROM trees_east ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(" + myLocation.lng + "," + myLocation.lat + "), 4326) LIMIT 5";
+  } else {
+    sqlQueryClosestTrees = "SELECT * FROM trees_east WHERE common_name = " + $('#common_name_list').val() + "ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(" + myLocation.lng + "," + myLocation.lat + "), 4326) LIMIT 5";
+  }
+  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + sqlQueryClosestTrees, function(data) {
+    nwTrees = L.geoJson(data, {
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+  });
+}
+
+function showAll() {
+  maybeClearLayers();
+  // Get CARTO selection as GeoJSON & add to map
+  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + queryAllTrees, function(data) {
+    nwTrees = L.geoJson(data, {
+      onEachFeature: onEachFeature,
+    });
+    clusters = L.markerClusterGroup({
+      spiderfyOnMaxZoom: false,
+      disableClusteringAtZoom: 18,
+    });
+    clusters.addLayer(nwTrees);
+    map.addLayer(clusters);
+  });
+}
+
+function showSweetgum() {
+  maybeClearLayers();
+  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + querySweetgumTrees, function(data){
+    nwTrees = L.geoJson(data, {
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+  });
+}
+// Event listeners
+$('input[value=SWEETGUM]').click(function() {
+  showSweetgum();
+});
+$('input[value=all]').click(function() {
+  showAll();
+});
--- script.js
+++ script.js
@@ -113,10 +113,6 @@ var greenIcon = L.icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png'
 })
 
-var cartoDBUserName = "saintaardvark";
-var queryAllTrees = "SELECT * FROM trees_east";
-var querySweetgumTrees = "SELECT * FROM trees_east WHERE common_name='SWEETGUM'";
-var queryAllTreeCommonNames = "SELECT DISTINCT common_name FROM trees_east ORDER BY common_name";
 function locateUser() {
   map.locate({setView: true, maxZoom: 17});
 }
@@ -150,78 +146,6 @@ function populateMenuWithAllTreeCommonNames() {
   });
 }
 
-function selectTreesMatchingCommonName(common_name) {
-  maybeClearLayers();
-  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + queryAllTrees + " WHERE common_name = '" + common_name + "'", function(data) {
-    nwTrees = L.geoJson(data, {
-      onEachFeature: function (feature, layer) {
-	if (feature.properties) {
-	  popupMsg = "Genus: "  + feature.properties.genus
-	    + "<br>Cultivar: " + feature.properties.cultivar
-	    + "<br>Species: " + feature.properties.species
-	    + "<br>Scientific Name: " + feature.properties.scientific_name
-	    + "<br>Common Name: " + feature.properties.common_name;
-	  layer.bindPopup(popupMsg);
-	}
-      }
-    });
-    clusters = L.markerClusterGroup({
-      spiderfyOnMaxZoom: false,
-      disableClusteringAtZoom: 18,
-    });
-    clusters.addLayer(nwTrees);
-    map.addLayer(clusters);
-    nwTrees.addTo(map);
-  });
-}
-
-function closestTree() {
-  maybeClearLayers();
-
-  var sqlQueryClosestTrees;
-  if ($('#common_name_list').val() == "") {
-    sqlQueryClosestTrees = "SELECT * FROM trees_east ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(" + myLocation.lng + "," + myLocation.lat + "), 4326) LIMIT 5";
-  } else {
-    sqlQueryClosestTrees = "SELECT * FROM trees_east WHERE common_name = " + $('#common_name_list').val() + "ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(" + myLocation.lng + "," + myLocation.lat + "), 4326) LIMIT 5";
-  }
-  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + sqlQueryClosestTrees, function(data) {
-    nwTrees = L.geoJson(data, {
-      onEachFeature: onEachFeature,
-    }).addTo(map);
-  });
-}
-
-function showAll() {
-  maybeClearLayers();
-  // Get CARTO selection as GeoJSON & add to map
-  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + queryAllTrees, function(data) {
-    nwTrees = L.geoJson(data, {
-      onEachFeature: onEachFeature,
-    });
-    clusters = L.markerClusterGroup({
-      spiderfyOnMaxZoom: false,
-      disableClusteringAtZoom: 18,
-    });
-    clusters.addLayer(nwTrees);
-    map.addLayer(clusters);
-  });
-}
-
-function showSweetgum() {
-  maybeClearLayers();
-  $.getJSON("https://" + cartoDBUserName + ".carto.com/api/v2/sql?format=GeoJSON&q=" + querySweetgumTrees, function(data){
-    nwTrees = L.geoJson(data, {
-      onEachFeature: onEachFeature,
-    }).addTo(map);
-  });
-}
-// Event listeners
-$('input[value=SWEETGUM]').click(function() {
-  showSweetgum();
-});
-$('input[value=all]').click(function() {
-  showAll();
-});
 
 
 // Only initialize dropdown lists if all data was successfully fetched.
@@ -244,3 +168,7 @@ $( document ).ready(function() {
   });
   showAll();
 })
+
+$('input[value=all]').click(function() {
+  showAll();
+});
